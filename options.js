30044fde25eae1265c0fdef0d6157521d7991f1d
Sneha Damodaran <48755269+snehadamodaran1@users.noreply.github.com>
2022-07-26 10:39:32 +0530

    budget manager commit

---
+++ options.js
@@ -0,0 +1,21 @@
+$(function() {
+    $('#saveLimit').click(function() {
+        var limit = $('#limit').val();
+        if(limit){
+            chrome.storage.sync.set({'limit' : limit},function() {
+                close();
+            });
+        }
+    });
+    $('#resetTotal').click(function() {
+        chrome.storage.sync.set({'total':0},function() {
+            var notifOptions = {
+                type : 'basic',
+                iconUrl : 'icon48.png',
+                title : 'Total reset.',
+                message : "Total has been reset to 0."
+            };
+            chrome.notifications.create('limitNotif',notifOptions);
+        });
+    });
+});
